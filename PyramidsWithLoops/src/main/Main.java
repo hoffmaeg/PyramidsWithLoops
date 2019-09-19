ed87d6e299ed59a3330542e051003e37224b7963
hoffmaeg <hoffmaeg@LCBLAB2110-38.uccob.uc.edu>
2019-09-19 09:19:01 -0400

    edits after class 9/19

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -15,11 +15,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		int a = lines + 4 - 1;
+	
+		for (int i = 1; i <= lines; i++) {
+			
+			for (int j = a; j > 0; j--) {
+				System.out.print(" ");
+			} for (int k = 1; k <= i; k++) {
+			System.out.print(i + " ");
 			}
-			System.out.println("");
+			System.out.println();
+			a++;
+			
+		/*int rows = 1;
+		for (int i = lines; i > 0; i--) {
+			for (int j = 1; j > 0; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= rows; k++) {
+				System.out.print(rows + "");
+			}
+			System.out.println();
+			
+			rows++;*/
+		
 		}
+	}
+
+	private static void printSpaces(int i) {
+		// TODO Auto-generated method stub
+		
 	}
 }
